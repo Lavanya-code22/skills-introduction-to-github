0c7aa9e49dd9c6620e36f23f2b952b69a0110dae
Lavanya-code22 <paladugulavanya21@gmail.com>
2025-06-04 10:52:46 +0530

    Add files via upload

---
+++ Priority Queue.java
@@ -0,0 +1,14 @@
+//priority queue
+import java.util.PriorityQueue;
+public class PriorityQueue{
+public static void main(String[]args){
+Priority Queue<Integer>pq=new Priority Queue<>((a,b)->b-a);
+max pq.offer(30);
+max pq.offer(10);
+max pq.offer(20);
+System.out.println("Priority Queue(max-Heap):");
+while(!maxpq.is Empty()){
+System.out.println(maxpq.poll());
+}
+}
+}
